6b2db61c008038c786fd27f7ba4d6d9302156931
Fabiano Shimura <fabiano.shimura@usp.br>
2022-04-27 14:13:56 -0300

    echo 1 para selecionar renovacao do certificado

--- renew_certificates_2022.sh
+++ renew_certificates_2022.sh
@@ -15,11 +15,9 @@ systemctl start nginx
 sudo certbot --nginx -d observatorio.smped.prefeitura.sp.gov.br -d adm.observatorio.smped.prefeitura.sp.gov.br -d api.observatorio.smped.prefeitura.sp.gov.br -d configurador.observatorio.smped.prefeitura.sp.gov.br -d graficos.observatorio.smped.prefeitura.sp.gov.br -d paineis.observatorio.smped.prefeitura.sp.gov.br
 
 #Attempt to reinstall this existing certificate
-1
-
+echo 1
 
 # Parar nginx para desocupar a porta de rede http para o container do proxy
-
 sudo systemctl stop nginx
 sudo systemctl disable nginx
 
@@ -29,7 +27,7 @@ sudo systemctl disable nginx
 /bin/cp --force   /etc/letsencrypt/live/observatorio.smped.prefeitura.sp.gov.br-0001/privkey.pem /root/implantacao_2020_08_18/ObservatorioSMPED/proxy/data/certbot/conf/live/all/
 
 # Iniciar o container do proxy
-
+cd 
 cd implantacao_2020_08_18
 cd ObservatorioSMPED
 cd proxy
